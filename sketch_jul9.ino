71be8ec3cf3f39c8a554ff2c4aa5ac061e12f78a
Svanik Nair <svaniknair05@gmail.com>
2026-07-09 13:13:17 -0500

    .

--- sketch_jul9.ino
+++ sketch_jul9.ino
@@ -6,15 +6,15 @@
 #define TFT_MOSI  20 // Pin assignments
 #define TFT_SCLK  21
 #define TFT_RST   10
-Adafruit_ST7789 display = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST); // screen object thingy
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST); // screen object thingy
 
 int hours = 12; // set it up at exactly midnight for it to work
 int minutes = 0;
 
 void setup() {
   Serial.begin(115200); 
-  display.init(76, 284);  
-  display.fillScreen(ST77XX_BLACK); // oi screen wake up matey boi
+  tft.init(76, 284);  
+  tft.fillScreen(ST77XX_BLACK); // oi screen wake up matey boi
 }
 
 void loop() {
@@ -24,9 +24,9 @@ void loop() {
     hourse = (hours % 12) + 1;
   }  
 }
-  display.fillScreen(ST77XX_RED); // wanted red font so it looks cool
-  display.setCursor(38, 142); // not sure might be the middle
-  display.setTextSize(5); // text might be a little large can adjust later
-  display.printf(hours, minutes); // show the time on the screen
+  tft.fillScreen(ST77XX_RED); // wanted red font so it looks cool
+  tft.setCursor(38, 142); // not sure might be the middle
+  tft.setTextSize(5); // text might be a little large can adjust later
+  tft.printf(hours, minutes); // show the time on the screen
   delay(60000)
 }
